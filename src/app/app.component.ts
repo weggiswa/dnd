45dbc767b175ee7124cc4b1805a2270f66057331
Dominik Weckbach <dominikweckbach@hotmail.de>
2019-01-18 15:13:33 +0100

    initial commit with initial layout

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  screenWidth: any
+  screenHeight: any
+
+
+  constructor() {
+    this.getScreenSize();
+
+  }
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?) {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+    console.log(this.screenHeight, this.screenWidth);
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GameboardComponent } from './gameboard/gameboard.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GameboardComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
